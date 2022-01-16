d832f84645cd0519cf61b5678faf0bbf641f351d
Ebru Rice <ebruerenb@gmail.com>
2022-01-16 14:59:49 -0800

    Add makeApiCall function and ui logic

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import CurrencyExchange from './script.js';
+
+
+async function makeApiCall(currency) {
+  const response = await CurrencyExchange.getCurrency(currency);
+  getElements(response);
+}
+
+function getElements(respone){
+
+}
+
+$(document).ready(function() {
+  $('#convert').click(function() {
+    let amount = $('#money').val();
+    let currency = $('#currency').val();
+    makeApiCall(currency);
+  });
+});
